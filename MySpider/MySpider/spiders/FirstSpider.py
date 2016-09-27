4a7d2b55ff4e1753ffeaab9804b69c898d091fac
xinXQ <blrsqing@163.com>
2016-09-27 17:40:34 +0800

    aa

---
+++ MySpider/MySpider/spiders/FirstSpider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+
+    def __init__(self, category=None, *args, **kwargs):
+        super(MySpider, self).__init__(*args, **kwargs)
+        self.start_urls = ['http://sports.ifeng.com/listpage/31193/1/list.shtml']
+
+    def parse(self, response):
+        fw = open('./result.html')
+        fw.write(response.body)
